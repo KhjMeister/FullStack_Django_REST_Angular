093e4f3753bc225335e3ceeda44155ef92cf9c2d
unknown <khaledjamal5946@gmail.com>
2022-07-13 02:18:28 -0700

    first angular start

--- Backend/config/config/settings.py
+++ Backend/config/config/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
 
     'rest_framework_simplejwt',
 ]
